24f82bea9e9bd21d4f74f603c4b313df6130c45f
imryan <notryancohen@gmail.com>
2016-03-22 12:08:29 -0400

    added NSLog() call

--- your-first-NSLog/FISAppDelegate.m
+++ your-first-NSLog/FISAppDelegate.m
@@ -5,15 +5,10 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    /**
     
-     * Hey! Write your code here! :D
-     
-     */
+    NSLog(@"hello, world!");
     
-    // do not alter
-    return YES;  //
-}   ///////////////
+    return YES;
+}
 
 @end
